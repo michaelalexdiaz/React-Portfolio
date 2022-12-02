0e2ca0e8bc8961fb82f9cfeb29a93a1318b26708
michaelalexdiaz <michaelalexdiaz@gmail.com>
2022-12-02 14:02:53 -0600

    creating contact component

--- main/react-portfolio-mikealexdiaz/src/components/contact/contact.js
+++ main/react-portfolio-mikealexdiaz/src/components/contact/contact.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormControl';
+import Input from '@mui/material/Input';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { Button } from '@mui/material';
 
 const contact = () => {
     return (
@@ -9,6 +16,47 @@ const contact = () => {
                 <h1>Contact</h1>
             </Grid>
         </Grid>
+        <Typography align="center">
+        <Grid container justifyContent='center'>
+            <Grid item xs={12} md={12}>
+            <FormControl>
+                <InputLabel htmlFor="my-input">Email address</InputLabel>
+                     <Input id="my-input" aria-describedby="my-helper-text" />
+                <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            </FormControl>
+            </Grid>
+            <Grid item xs={12} md={12}>
+            <FormControl>
+                <InputLabel htmlFor="my-input">Email address</InputLabel>
+                     <Input id="my-input" aria-describedby="my-helper-text" />
+                <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            </FormControl>
+            </Grid>
+            <Grid item xs={12} md={12}>
+            <FormControl>
+                <InputLabel htmlFor="my-input">Email address</InputLabel>
+                     <Input id="my-input" aria-describedby="my-helper-text" />
+                <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            </FormControl>
+            </Grid>
+        </Grid>
+        <Grid item xs={12} md={12}>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Grid item xs={4} md={4}></Grid>
+            <TextField
+            id="outlined-multiline-static"
+            label="Multiline"
+            multiline
+            rows={4}
+            defaultValue="Default Value"
+            />
+        </Grid>
+        <Grid item border="solid" xs={12} md={12}>
+        <Button color="primary">
+                <p>Submit</p>
+        </Button>
+        </Grid>
+        </Typography>
         </>
     )
 }
